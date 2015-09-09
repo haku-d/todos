a33c55a47929a3ff96814deabe720813f7f4ce70
ThanhDc <clgt@clgt.vn>
2015-09-09 09:13:24 +0700

    Update app body html

--- client/app.js
+++ client/app.js
@@ -1,11 +1,12 @@
+/**
+ * Main app body template helper
+ */
 Template.appBody.helpers({
-  
   thisArray: function() {
     return [this];
   },
-
+  // List of projects
   projects: function() {
     return Projects.find();
   }
-  
 });
